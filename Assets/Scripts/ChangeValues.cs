f21248d28362530df0c397b2b4aa5b01eb08cb20
Caleb Brown <caleb.matthew.brown@gmail.com>
2016-11-15 14:26:30 -0500

    First Commit

---
+++ Assets/Scripts/ChangeValues.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeValues : MonoBehaviour
+{
+    public GameObject Quad;
+
+    public void IterationsValue(float newValue)
+    {
+        Renderer renderer = Quad.GetComponent<Renderer>();
+        Material mat = renderer.material;
+        mat.SetFloat("_Iterations", newValue);
+    }
+
+    public void ZoomValues(float newValue)
+    {
+        Renderer renderer = Quad.GetComponent<Renderer>();
+        Material mat = renderer.material;
+        mat.SetVector("_Zoom", new Vector4(newValue, newValue, 1, 1));
+    }
+
+    public void PanValues(float newValue)
+    {
+        Renderer renderer = Quad.GetComponent<Renderer>();
+        Material mat = renderer.material;
+        mat.SetVector("_Pan", new Vector4(newValue, 0, 1, 1));
+    }
+}
